c7f28b2f444f247a6c56d871b0c61204a2e24e35
solom.wm <solom.on.wm00017@gmail.com>
2018-10-27 00:02:27 +0300

    WebService Beta 1 Ready

--- TurretWebServiceCore/TurretWebServiceCore/Controllers/HomeController.cs
+++ TurretWebServiceCore/TurretWebServiceCore/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TurretWebServiceCore.Models;
 
@@ -12,15 +6,11 @@ namespace TurretWebServiceCore.Controllers
 {
     public class HomeController : Controller
     {
-        //[Authorize(Roles = "administrator, user")]
         public IActionResult Index()
         {
-            //string role = User.FindFirst(x => x.Type == ClaimsIdentity.DefaultRoleClaimType).Value;
-            //return Content(User.Identity.Name + $"\nYour role is {role}");
             return View();
         }
 
-        //[Authorize(Roles = "administrator")]
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
