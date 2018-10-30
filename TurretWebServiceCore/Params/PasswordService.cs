dfcaf1dc3154dce1708154ba5225f0ff944c53be
solom.wm <solom.on.wm00017@gmail.com>
2018-10-31 00:32:23 +0300

    Написан класс PasswordService, который возврвщает хэш-код пароля.

---
+++ TurretWebServiceCore/Params/PasswordService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Params
+{
+    public static class PasswordService
+    {
+        public static string GetPasswordHash(string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            return BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(password))).Replace("-", string.Empty);
+        }
+    }
+}
