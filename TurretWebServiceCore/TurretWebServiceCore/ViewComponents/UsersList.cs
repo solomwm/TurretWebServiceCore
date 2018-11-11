fc2c2c518981ec5340dba5fce38387ebda6bfd80
solom.wm <solom.on.wm00017@gmail.com>
2018-11-12 00:45:45 +0300

    Представление для Top Users List

---
+++ TurretWebServiceCore/TurretWebServiceCore/ViewComponents/UsersList.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TurretWebServiceCore.Data;
+
+namespace TurretWebServiceCore.ViewComponents
+{
+    public class UsersList: ViewComponent
+    {
+        private readonly TurretDBContext db;
+
+        public UsersList(TurretDBContext dBContext)
+        {
+            db = dBContext;
+        }
+
+        public IViewComponentResult Invoke(int topCount)
+        {
+            IEnumerable<TurretWebServiceCore.Models.User> users = db.Users;
+            users = users.OrderByDescending(u => u.MaxScore).Take(topCount);
+            return View("_TopUsers", users);
+        }
+    }
+}
