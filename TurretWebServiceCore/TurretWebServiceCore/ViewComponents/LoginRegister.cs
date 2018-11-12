fcae8fd44df9759591f6a5ad929879887ed0d5f7
solom.wm <solom.on.wm00017@gmail.com>
2018-11-12 23:50:39 +0300

    Добавление частичных представлений LoginRegister и WelcomeLogout.

---
+++ TurretWebServiceCore/TurretWebServiceCore/ViewComponents/LoginRegister.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TurretWebServiceCore.ViewComponents
+{
+    public class LoginRegister: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("_LoginRegister");
+        }
+    }
+}
---
+++ TurretWebServiceCore/TurretWebServiceCore/ViewComponents/WelcomeLogout.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TurretWebServiceCore.ViewComponents
+{
+    public class WelcomeLogout: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("_WelcomeLogout", User.Identity.Name);
+        }
+    }
+}
